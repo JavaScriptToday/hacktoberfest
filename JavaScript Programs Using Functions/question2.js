943f49ee7bb767471874a6e37c24725c840378a9
Arjun Gupta <arjun011gupta@gmail.com>
2023-10-06 21:20:54 +0530

    JAVASCRIPT PROGRAMS

---
+++ JavaScript Programs Using Functions/question2.js
@@ -0,0 +1,12 @@
+// Function with Parameters
+// program to print the text
+// declaring a function
+function greet(name) {
+    console.log("Hello " + name + ":)");
+}
+
+// variable name can be different
+let name = prompt("Enter a name: ");
+
+// calling function
+greet(name);
